7d4e4561f91e1362a67188bc430f817d028a8724
ArthurBerto <arthur.berto.ferreira@gmail.com>
2025-04-01 19:12:12 -0300

    Finalizando a estrutura do projeto

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const rxphbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 const session = require("express-session");
 const FileStore = require("session-file-store")(session);
 const flash = require("express-flash");
@@ -7,6 +7,55 @@ const flash = require("express-flash");
 const app = express();
 
 const conn = require("./db/conn");
+
+// Template engine
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+// Receber resposta do body
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+// Session Middleware
+app.use(
+  session({
+    name: "session", // Nome da Seção
+    secret: "nosso_secrett",
+    resave: "false", // Se cair a seção, o usuário deve reconectar
+    saveUninitialized: false,
+    store: new FileStore({
+      // Salvar seções em arquivos no servidor
+      logFn: function () {},
+      path: require("path").join(require("os").tmpdir(), "sessions"), // Caminho para a pasta sessions
+    }),
+    cookie: {
+      secure: false,
+      maxAge: 360000, // Tempo de duração do cookie -> 1 dia
+      expires: new Date(Date.now() + 360000), // Expira em 1 dia também
+      httpOnly: true, //Sem certificado de segurança, pois usa o localhost
+    },
+  })
+);
+
+// Flash Messages
+app.use(flash());
+
+// Public Path
+app.use(express.static("public"));
+
+// Set session to res
+app.use((req, res, next) => {
+  if (req.session.userid) { // Se o usuário está logado, a sessão é enviada para a resposta
+    res.locals.session = req.session;
+  }
+
+  next()
+});
 
 // Banco de dados ouvindo a porta 3000
 conn
