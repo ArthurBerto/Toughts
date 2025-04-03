206ba98016842fd1d9fc9c8ef181ca5e0009466c
ArthurBerto <arthur.berto.ferreira@gmail.com>
2025-04-03 17:07:38 -0300

    Registrando users

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,3 +1,7 @@
+const User = require("../models/User");
+
+const bcrypt = require("bcryptjs");
+
 module.exports = class AuthController {
   static login(req, res) {
     res.render("auth/login");
@@ -5,5 +9,52 @@ module.exports = class AuthController {
 
   static register(req, res) {
     res.render("auth/register");
+  }
+
+  static async registerPost(req, res) {
+    const { name, email, password, confirmpassword } = req.body;
+
+    // Validação de senha
+    if (password != confirmpassword) {
+      // Mensagem para o front
+      req.flash("message", "As senhas não conferem, tente novamente!");
+      res.render("auth/register");
+
+      return;
+    }
+
+    // Checar se o usuário exite
+    const checkIfUserExists = await User.findOne({ where: { email: email } });
+
+    if (checkIfUserExists) {
+      // Mensagem para o front
+      req.flash("message", "O e-mail já está em uso!");
+      res.render("auth/register");
+
+      return;
+    }
+
+    // Criar a senha
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    const user = {
+      name,
+      email,
+      password: hashedPassword,
+    };
+
+    try {
+      const createdUser = await User.create(user);
+
+      // Inicializa a sessão
+      req.session.userid = createdUser.id;
+      req.flash("message", "Cadastro realizado com sucesso!");
+      req.session.save(() => {
+        res.redirect("/");
+      });
+    } catch (err) {
+      console.log(err);
+    }
   }
 };
