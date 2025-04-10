a8eba1d5d971cda9dc6f721f156d6bc885924471
ArthurBerto <arthur.berto.ferreira@gmail.com>
2025-04-10 20:50:36 -0300

    Preenchendo forms de edição

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -70,4 +70,12 @@ module.exports = class ToughtsController {
       console.log(`Aconteceu um erro: ${err}`);
     }
   }
+
+  static async updateTought(req, res) {
+    const id = req.params.id;
+
+    const tought = await Tought.findOne({ where: { id: id }, raw: true });
+
+    res.render("toughts/edit", { tought })
+  }
 };
