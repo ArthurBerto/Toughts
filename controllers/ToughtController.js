bca242470f74bd98837b24deb6123d82b7a531b7
ArthurBerto <arthur.berto.ferreira@gmail.com>
2025-04-10 20:55:58 -0300

    Atualização da edição do pensamento

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -76,6 +76,26 @@ module.exports = class ToughtsController {
 
     const tought = await Tought.findOne({ where: { id: id }, raw: true });
 
-    res.render("toughts/edit", { tought })
+    res.render("toughts/edit", { tought });
+  }
+
+  static async updateToughtSave(req, res) {
+    const id = req.body.id;
+
+    const tought = {
+      title: req.body.title,
+    };
+
+    try {
+      await Tought.update(tought, { where: { id: id } });
+
+      req.flash("message", "Pensamento atualizado com sucesso!");
+
+      req.session.save(() => {
+        res.redirect("/toughts/dashboard");
+      });
+    } catch (err) {
+      console.log(`Aconteceu um erro: ${err}`);
+    }
   }
 };
